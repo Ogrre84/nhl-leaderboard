aca8e3050c94106148d054f46d9778f67b177549
Ian Douglas <ogrre1984@gmail.com>
2024-10-08 20:18:57 -0700

    updated path to lastScrapModel

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-const LastScrape = require('./models/lastScrapeModel')
+const LastScrape = require('./API/models/lastScrapeModel')
 require('dotenv').config();
 
 const app = express();
